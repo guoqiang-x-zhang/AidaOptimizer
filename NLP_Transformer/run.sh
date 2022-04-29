2aaaba4debda4fa6283c5669f9c439577ba7cda1
guoqiang-x-zhang <104566710+guoqiang-x-zhang@users.noreply.github.com>
2022-04-29 09:59:29 +0200

    Update run.sh

--- NLP_Transformer/run.sh
+++ NLP_Transformer/run.sh
@@ -1,5 +1,3 @@
-python train.py  --optim aida  --eps 1e-16 -output_dir output_aida_1e_16_0  --run 0 -data_pkl m30k_deen_shr.pkl -embs_share_weight -proj_share_weight -label_smoothing  -b 200 -warmup 128000 -epoch 400  
-
-
-
-
+python train.py  --optim aida  --eps 1e-16 -output_dir output_aida_1e_16_0  --run 0 -data_pkl m30k_deen_shr.pkl -embs_share_weight -proj_share_weight -label_smoothing  -b 200 -warmup 128000 -epoch 400 
+python train.py  --optim adabelief  --eps 1e-16 -output_dir output_adabelief_1e_16_0  --run 0 -data_pkl m30k_deen_shr.pkl -embs_share_weight -proj_share_weight -label_smoothing  -b 200 -warmup 128000 -epoch 400 
+python train.py  --optim adam  --eps 1e-9 -output_dir output_adam_1e_9_0  --run 0 -data_pkl m30k_deen_shr.pkl -embs_share_weight -proj_share_weight -label_smoothing  -b 200 -warmup 128000 -epoch 400  
